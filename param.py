6d25964d4631c6ce5e600a2ea7af1a24996148ce
Valentin Brosseau <c4software@gmail.com>
2017-05-01 21:57:35 +0200

    Complete example

--- param.py
+++ param.py
@@ -1,6 +1,8 @@
-sub_menu_1 = {"List": [{"1": {"Item": "test@192.168.1.1"}}]}
+sub_menu_1 = {"List 1": [{"1": {"Item 1": "test@192.168.1.1"}}, {"2": {"Item 2": "test@192.168.1.1"}}]}
+sub_menu_2 = {"List 2": [{"1": {"Item": "test@192.168.1.1"}}]}
 
 menu = [
     {"1": sub_menu_1},
-    {"2": {"Item": "test@192.168.1.1"}}
+    {"2": sub_menu_2},
+    {"3": {"Item": "test@192.168.1.1"}}
 ]
